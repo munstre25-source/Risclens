83fb1992f4c3b96cdc4c3d178e90331c0b688068
KevQPSA <mailrafo23@gmail.com>
2026-01-09 17:40:04 +0300

    theme fix

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
+import * as React from 'react';
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: ThemeProviderProps & { children: React.ReactNode }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
